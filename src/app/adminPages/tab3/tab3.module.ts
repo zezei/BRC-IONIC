dc26c9b7dbfb1f3b8ed16bb8e20a57f4b02e5bde
Alesandro <catgolondrinas@gmail.com>
2020-06-10 15:55:00 -0300

    Initial commit

--- src/app/adminPages/tab3/tab3.module.ts
+++ src/app/adminPages/tab3/tab3.module.ts
@@ -1,10 +1,8 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module'
 
@@ -13,8 +11,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module'
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
   ],
   declarations: [Tab3Page]
